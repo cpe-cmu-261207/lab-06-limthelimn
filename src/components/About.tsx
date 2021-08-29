7585c47501c49e62fc083e9a99f13375472b3abf
limthelimn <nuttawan.lim@gmail.com>
2021-08-29 20:16:52 +0700

    all done

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const about = () => {
+    return (
+        <div className ='text-center space-y-3'>
+            <p className ='text-2xl font-semibold'>About me</p>
+            <p className ='text-xl'>Nuttawan Reabreang 630612099</p>
+        </div>
+    )
+}
+
+export default about
